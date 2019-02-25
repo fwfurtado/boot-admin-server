b13e07502a3fe08898b65aaf27024d5f87838cf9
Fernando <feh.wilinando@gmail.com>
2019-02-25 10:36:21 -0300

    Async Event publisher

---
+++ src/main/java/com/github/fehwilinando/bootadminserver/BootAdminServerApplication.java
@@ -0,0 +1,14 @@
+package com.github.fehwilinando.bootadminserver;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableAdminServer
+@SpringBootApplication
+public class BootAdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootAdminServerApplication.class, args);
+    }
+}
